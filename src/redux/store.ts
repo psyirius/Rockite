8bc861310339514559a2821d133ba7a27c064633
psyirius <psyirius@gmail.com>
2024-12-02 12:33:50 -0800

    Minor Fixes

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,4 +1,4 @@
-import { applyMiddleware, compose, createStore } from 'redux';
+import { applyMiddleware, compose, legacy_createStore as createStore } from 'redux';
 import thunk from 'redux-thunk';
 import reducer from './reducers';
 import persistence from './persistence/persistence-middleware';
