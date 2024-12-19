524eb8234500b66e155593a77840c9af0b0aeca1
psyirius <psyirius@gmail.com>
2024-12-19 09:04:36 -0800

    ui: enhanced history view

--- apps/web/src/components/Connections/Events/EventRow.tsx
+++ apps/web/src/components/Connections/Events/EventRow.tsx
@@ -1,125 +1,169 @@
 import { cn } from '$lib/utils'
 import type Event from '$models/event'
 import { EventPayloadType, EventType } from '$models/event'
-import { format } from 'date-fns'
-import { Binary as LuBinary, Text as LuText } from 'lucide-react'
-import { useState } from 'react'
-import CopyToClipboard from 'react-copy-to-clipboard'
-import { FaArrowDown, FaArrowUp, FaRegCopy, FaRegEdit } from 'react-icons/fa'
-import { MdDone } from 'react-icons/md'
+import {
+  Binary as LuBinary,
+  Text as LuText,
+  Clipboard as LuClipboard,
+  ClipboardCheck as LuClipboardCheck
+} from 'lucide-react'
+import { FaArrowDown, FaArrowUp, FaRegEdit } from 'react-icons/fa'
 import { RiInformationLine } from 'react-icons/ri'
+import { useState } from 'react'
+import { format } from 'date-fns'
+import { Button } from '@headlessui/react'
 import EventRowPayload from './EventRowPayload'
+import {toast} from "sonner";
 
 export interface EventRowProps {
   event: Event
   shouldFormatPayload: boolean
   onOpenInNewTab: () => void
-  layout: 'narrow' | 'wide'
+  layout: 'narrow' | 'wide' // TODO: implement
 }
 
-export default function EventRow({ event, shouldFormatPayload, onOpenInNewTab, layout }: EventRowProps) {
+export default function EventRow({ event, shouldFormatPayload, onOpenInNewTab }: EventRowProps) {
   const [copiedToClipboard, setCopiedToClipboard] = useState(false)
 
+  async function copyToClipboard(text: string) {
+    if (!copiedToClipboard) {
+      try {
+        await navigator.clipboard.writeText(text)
+        setCopiedToClipboard(true)
+        setTimeout(() => setCopiedToClipboard(false), 2000)
+      } catch (err) {
+        toast.error('Failed to copy text!')
+      }
+    }
+  }
+
   return (
-    <div
-      key={event.id}
-      className={cn(
-        'group',
-        'max-w-full flex flex-row justify-between py-1 px-4 hover:bg-gray-100 hover:dark:bg-gray-800',
-        layout === 'narrow' && 'flex-wrap',
-      )}
-    >
-      <div className="order-1 flex flex-shrink-0">
-        {/* Timestamp */}
-        <div className="text-gray-400 dark:text-gray-600 font-mono">
-          {format(new Date(event.timestamp), 'HH:mm ss.SS')}
-        </div>
-
-        {/* Icon */}
-        {event.type === EventType.Sent && (
-          <div className="text-green-500 dark:text-green-200 ml-2 p-1 text-xs">
-            <FaArrowUp title="Sent payload" />
-          </div>
-        )}
-
-        {event.type === EventType.Received && (
-          <div className="text-red-500 dark:text-red-200 ml-2 p-1 text-xs">
-            <FaArrowDown title="Received payload" />
-          </div>
-        )}
-
-        {event.type === EventType.Meta && (
-          <div className="text-gray-500 dark:text-gray-700 ml-2 p-1 text-xs">
-            <RiInformationLine title="Information" />
-          </div>
-        )}
-      </div>
-
-      {/* Payload */}
-      <div
-        className={cn(
-          'flex-grow min-w-0 font-mono',
-          layout === 'narrow' && 'order-4 w-full',
-          layout === 'wide' && 'order-3 ml-4',
-          event.type === EventType.Sent && 'text-green-900 dark:text-green-200',
-          event.type === EventType.Received && 'text-red-900 dark:text-red-200',
-          event.type === EventType.Meta && 'text-gray-900 dark:text-gray-200',
-        )}
-      >
-        <pre className="whitespace-pre-wrap break-words">
-          <EventRowPayload event={event} shouldFormatPayload={shouldFormatPayload} />
-        </pre>
-      </div>
-
-      <div
-        className={cn(
-          // 'invisible group-hover:visible',
-          event.type === EventType.Sent && 'text-green-500 dark:text-green-200',
-          event.type === EventType.Received && 'text-red-500 dark:text-red-200',
-          layout === 'narrow' && 'order-3',
-          layout === 'wide' && 'order-4',
-        )}
-      >
-        {event.payloadType === EventPayloadType.Text && (
-          <div className="bg-gray-100 dark:bg-gray-800 rounded p-1 text-xs" title="Text">
-            <LuText size={14} />
-          </div>
-        )}
-
-        {event.payloadType === EventPayloadType.Binary && (
-          <div className="bg-gray-100 dark:bg-gray-800 rounded p-1 text-xs" title="Binary">
-            <LuBinary size={14} />
-          </div>
-        )}
-      </div>
-
-      <div
-        className={cn(
-          'invisible group-hover:visible text-gray-400',
-          layout === 'narrow' && 'order-3',
-          layout === 'wide' && 'order-4',
-        )}
-      >
-        <CopyToClipboard
-          text={event.payload}
-          onCopy={() => {
-            if (!copiedToClipboard) {
-              setCopiedToClipboard(true)
-              setTimeout(() => setCopiedToClipboard(false), 2000)
-            }
-          }}
+    <div className={cn(
+      'group',
+      "py-1",
+      "w-full max-w-full"
+    )}>
+      <div className="flex flex-col">
+        {/* Header */}
+        <div
+          className={cn(
+            'flex items-center',
+          )}
         >
-          <button type="button" className={cn('ml-2 hover:text-gray-600', copiedToClipboard && 'cursor-default')}>
-            {!copiedToClipboard && <FaRegCopy title="Copy to Clipboard" />}
-            {copiedToClipboard && <MdDone title="Copied to Clipboard" />}
-          </button>
-        </CopyToClipboard>
+          {/* Left */}
+          <div className="flex items-center flex-1 overflow-x-hidden space-x-1">
+            <div className="flex items-center justify-center flex-shrink-0">
+              {/* Timestamp */}
+              <div className="text-gray-400 dark:text-gray-600 font-mono">
+                {format(new Date(event.timestamp), 'HH:mm ss.SS')}
+              </div>
 
-        {event.type !== EventType.Meta && (
-          <button type="button" className="ml-2 hover:text-gray-600" onClick={() => onOpenInNewTab()}>
-            <FaRegEdit title="Open in Editor" />
-          </button>
-        )}
+              {/* Icon */}
+              {event.type === EventType.Sent && (
+                <div className="text-green-500 dark:text-green-200 ml-2 p-1 text-xs">
+                  <FaArrowUp title="Sent payload"/>
+                </div>
+              )}
+
+              {event.type === EventType.Received && (
+                <div className="text-red-500 dark:text-red-200 ml-2 p-1 text-xs">
+                  <FaArrowDown title="Received payload"/>
+                </div>
+              )}
+
+              {event.type === EventType.Meta && (
+                <div className="text-gray-500 dark:text-gray-700 ml-2 p-1 text-xs">
+                  <RiInformationLine title="Information"/>
+                </div>
+              )}
+            </div>
+
+            {/* Payload */}
+            <div className={cn(
+              "flex-1 overflow-hidden text-ellipsis whitespace-nowrap pr-2",
+              "min-w-0 font-mono max-w-full",
+              event.type === EventType.Sent && 'text-green-900 dark:text-green-200',
+              event.type === EventType.Received && 'text-red-900 dark:text-red-200',
+              event.type === EventType.Meta && 'text-gray-900 dark:text-gray-200',
+            )}>
+                <span className="">
+                  <EventRowPayload event={event} shouldFormatPayload={shouldFormatPayload}/>
+                </span>
+            </div>
+          </div>
+
+          {/* Badge & Controls */}
+          <div className={cn(
+            "flex items-center justify-end space-x-1",
+            "min-w-[26px]", // 22 + 4
+          )}>
+            <div
+              className={cn(
+                'group-hover:visible text-gray-400',
+                "flex items-center space-x-1",
+                'invisible size-0',
+                (event.type !== EventType.Meta) && 'group-hover:w-full',
+              )}
+            >
+              {event.type !== EventType.Meta && (
+                <Button
+                  className={cn(
+                    "rounded bg-sky-600 py-1 px-1 text-sm text-white",
+                    "data-[hover]:text-gray-400 data-[active]:text-gray-500",
+                    // "bg-red-300",
+                    // copiedToClipboard && 'cursor-default',
+                  )}
+                  onClick={(e) => {
+                    e.stopPropagation()
+                    copyToClipboard(event.payload)
+                  }}
+                  title={copiedToClipboard ? 'Copied to Clipboard' : 'Copy to Clipboard'}
+                >
+                  {copiedToClipboard
+                    ? <LuClipboardCheck size={14} />
+                    : <LuClipboard size={14} />
+                  }
+                </Button>
+              )}
+
+              {event.type !== EventType.Meta && (
+                <Button
+                  className={cn(
+                    "rounded bg-sky-600 py-1 px-1 text-sm text-white",
+                    "data-[hover]:text-gray-500 data-[active]:text-gray-600",
+                    // "bg-red-300",
+                  )}
+                  onClick={(e) => {
+                    e.stopPropagation()
+                    onOpenInNewTab()
+                  }}
+                >
+                  <FaRegEdit title="Open in Editor"/>
+                </Button>
+              )}
+            </div>
+
+            <div
+              className={cn(
+                // 'invisible group-hover:visible',
+                event.type === EventType.Sent && 'text-green-500 dark:text-green-200',
+                event.type === EventType.Received && 'text-red-500 dark:text-red-200',
+              )}
+            >
+              {event.payloadType === EventPayloadType.Text && (
+                <div className="bg-gray-100 dark:bg-gray-800 rounded p-1 text-xs" title="Text">
+                  <LuText size={14}/>
+                </div>
+              )}
+
+              {event.payloadType === EventPayloadType.Binary && (
+                <div className="bg-gray-100 dark:bg-gray-800 rounded p-1 text-xs" title="Binary">
+                  <LuBinary size={14}/>
+                </div>
+              )}
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
--- apps/web/src/components/Connections/Events/Events.tsx
+++ apps/web/src/components/Connections/Events/Events.tsx
@@ -7,6 +7,14 @@ import useResizeObserver from 'use-resize-observer/polyfilled'
 import EmptyMessage from '../../General/Utilities/EmptyMessage'
 import Heading from '../../General/Utilities/Heading'
 import EventRow from './EventRow'
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion"
+import {cn} from "$lib/utils.ts";
+import EventRowPayload from './EventRowPayload'
 
 export interface EventsProps {
   connection: Connection
@@ -35,17 +43,38 @@ export default function Events({ connection, formatEventPayloads, events, onClea
       </Heading>
       <div className="flex-grow relative flex" ref={containerRef}>
         {!!events.length && (
-          <div className="absolute inset-0 overflow-auto py-2 select-text">
+          <Accordion type="single" collapsible className="absolute inset-0 overflow-auto py-2 select-text">
             {events.map((event) => (
-              <EventRow
-                key={event.id}
-                event={event}
-                shouldFormatPayload={formatEventPayloads}
-                onOpenInNewTab={() => onCreateInTab(connection, event.payload)}
-                layout={containerWidth < 400 ? 'narrow' : 'wide'}
-              />
+              <AccordionItem value={event.id} key={event.id} className={cn(
+                "border-b-0 px-4 py-0.5 hover:bg-gray-100 hover:dark:bg-gray-800",
+              )}>
+                <AccordionTrigger className={cn(
+                  "p-0 hover:no-underline",
+                  "focus:!outline-0",
+                  'max-w-full',
+                )}>
+                  <div className="w-full pr-1 overflow-hidden">
+                    <EventRow
+                      event={event}
+                      shouldFormatPayload={formatEventPayloads}
+                      onOpenInNewTab={() => onCreateInTab(connection, event.payload)}
+                      layout={containerWidth < 400 ? 'narrow' : 'wide'}
+                    />
+                  </div>
+                </AccordionTrigger>
+                <AccordionContent className="pb-2">
+                  <div className={cn(
+                    "bg-gray-100 dark:bg-gray-900 p-2 rounded overflow-auto border dark:border-gray-700",
+                    "text-gray-50"
+                  )}>
+                    <pre className="font-mono text-sm dark:text-gray-200 text-gray-800 whitespace-pre-wrap break-words">
+                      <EventRowPayload event={event} shouldFormatPayload={formatEventPayloads}/>
+                    </pre>
+                  </div>
+                </AccordionContent>
+              </AccordionItem>
             ))}
-          </div>
+          </Accordion>
         )}
         {!events.length && (
           <EmptyMessage heading="No Output">Sent and received messages will show up here.</EmptyMessage>
